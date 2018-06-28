22b6f08415b4fcde987328d3afb82d4769757357
TaoMengyi <taomengyi@cslangtong.com>
2018-06-28 17:36:09 +0800

    angular中子提交

--- angular-seed-master/app/js/app.js
+++ angular-seed-master/app/js/app.js
@@ -2,9 +2,26 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+var app = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
+
+app.factory('hello1',function () {
+    return {
+        hello: function () {
+            console.log("hello1 service1!");
+        }
+    }
+});
+
+app.factory('hello2',function () {
+    return {
+        hello: function () {
+            console.log("hello2 service2!");
+        }
+    }
+});
+
--- angular-seed-master/app/js/controllers.js
+++ angular-seed-master/app/js/controllers.js
@@ -3,9 +3,17 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
+function MyCtrl1(hello1,hello2) {
+    hello1.hello();
+    hello2.hello();
+}
+MyCtrl1.$inject = ['hello1','hello2'];
+app.controller("partial1Ctr", function($scope,hello1,hello2){
+    $scope.hello = function(){
+        hello1.hello();
+        hello2.hello();
+    }
+});
 
 function MyCtrl2() {
 }
